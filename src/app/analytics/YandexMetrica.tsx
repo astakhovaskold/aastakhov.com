4f77d8a340ec22f916e6c8dcaa2a1a713686aeb1
Askold Astakhov <astakhovaskold@gmail.com>
2025-08-13 11:32:31 +0300

    add metrika

--- src/app/analytics/YandexMetrica.tsx
+++ src/app/analytics/YandexMetrica.tsx
@@ -5,18 +5,14 @@ const YandexMetrica = () => {
         <Script
             dangerouslySetInnerHTML={{
                 __html: `
-               (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-               m[i].l=1*new Date();
-               for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-               k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-               (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+                })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103735154', 'ym');
             
-               ym(99495024, "init", {
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    webvisor:true
-               });
+                ym(103735154, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
           `,
             }}
         />
