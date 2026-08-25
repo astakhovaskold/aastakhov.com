cf9cd3620056e3e1f6f4d0078519d3e4f6a7b8a3
Askold Astakhov <astakhovaskold@gmail.com>
2026-08-25 22:58:27 +0200

    avatar

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,9 @@ const nextConfig: NextConfig = {
       {
         pathname: '/api/media/file/**',
       },
+      {
+        pathname: '/askold-avatar.jpeg',
+      },
     ],
   },
   webpack: (webpackConfig) => {
